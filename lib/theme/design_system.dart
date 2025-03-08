a92d417cf645c2ab9e111a3ba143406e42de0a3e
Kelvin <kelvindelima710@gmail.com>
2025-03-08 02:27:48 -0300

    feat: create project design system and create InstantMessengerIllustrations class

---
+++ lib/theme/design_system.dart
@@ -0,0 +1,18 @@
+class InstantMessengerIllustrations {
+  InstantMessengerIllustrations();
+
+  // User status assets
+  static const String awayMini = 'assets/images/away-mini.svg';
+  static const String availableMini = 'assets/images/available-mini.svg';
+  static const String busyMini = 'assets/images/busy-mini.svg';
+  static const String offlineMini = 'assets/images/offline-mini.svg';
+  static const String awayMedium = 'assets/images/away-medium.svg';
+  static const String availableMedium = 'assets/images/available-medium.svg';
+  static const String busyMedium = 'assets/images/busy-medium.svg';
+  static const String offlineMedium = 'assets/images/offline-medium.svg';
+
+  // General icons assets
+  static const String inviteFriend = 'assets/images/invite-friend.png';
+  static const String exitMsn = 'assets/images/exit.png';
+  static const String settings = 'assets/images/settings.png';
+}
