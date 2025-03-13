38ea15f859dd7d0cc05ada0152e6d2271a46df1a
Kelvin <kelvindelima710@gmail.com>
2025-03-13 17:16:42 -0300

    feat: initializing firebase app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:instant_messenger_project/firebase_options.dart';
+import 'package:instant_messenger_project/pages/authpage.dart';
 import 'package:instant_messenger_project/pages/homepage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
@@ -10,6 +17,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Homepage());
+    return const MaterialApp(home: AuthPage());
   }
 }
